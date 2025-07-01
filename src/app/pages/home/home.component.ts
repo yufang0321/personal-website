719977cfce036510e49774f19876d6564b4af155
winniewang <claire10581@gmail.com>
2025-07-01 23:30:20 +0800

    refactor: 重構首頁內容

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -7,4 +7,16 @@ import { Component } from '@angular/core';
 })
 export class HomeComponent {
 
+  constructor() {
+
+  }
+
+  /**
+   * 導航至該指定標題位置
+   * @param targetId 
+   */
+  scrollTo(targetId: string): void {
+    const el = document.getElementById(targetId);
+    el?.scrollIntoView({ behavior: 'smooth' });
+  }
 }
